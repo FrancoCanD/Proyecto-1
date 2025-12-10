9e09d64cf82a7fc049d325b640b2a5887518b944
Franco <f.cancinodiaz@gmail.com>
2025-12-09 23:27:27 -0300

    Se agregan archivos script y se crean botones usando funciones de javascript

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+function mostrarexterno () {
+    alert('Mostrar más información desde un archivo js externo')
+}
---
+++ assets/js/scriptprueba.js
@@ -0,0 +1,7 @@
+const saludo = 'Hola'
+let numero = 2; 
+{
+    let alternador = 5;
+    let resultado = alternador + 50;
+    
+}
