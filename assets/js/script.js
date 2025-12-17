4ecab6c309035227b88c2b1aaf81cafe909d3b8f
FrancoCanD <f.cancinodiaz@gmail.com>
2025-12-16 22:58:02 -0300

    Se agragan funciones javascript, se crean botones interactivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ function mostrarexterno () {
 
 let miDivFila = document.getElementById('miDivFila');
 console.log(miDivFila);
-miDivFila.innerHTML = '<div class="col col-lg-2 border border-warning">Este div cambio de 3 a 1 caja';
+miDivFila.innerHTML = '<div class="col col-lg-2 border border-warning">Este div cambio de 3 a 1 caja</div>';
 
 let parrafoLorem = document.getElementById('lorem');
 parrafoLorem.innerText = 'Este es un texto modificado desde Javascript.';
@@ -15,37 +15,48 @@ parrafoLorem.style.fontSize = '18px';
 function mostrar(){
     const nombreInput = document.getElementById('nombre');
     const salida = document.getElementById('salida');
-
-    const nombre = nombreInput.value.trim();
+    const nombre = nombreInput.value;
     if (nombre.trim() === '') {
-        salida.innerText = 'Por favor, ingresa un nombre';
+        salida.innerText = 'Por favor, ingresa un nombre.';
         return;
-    } else{
+    }else{
         salida.innerText = 'Hola, ' + nombre + '!';
     }
 }
-
 let contador = 0;
 function formatear(){
     const nombre = document.getElementById('nombre').value.trim();
     const historial = document.getElementById('historial');
-
-    if (!nombre) return;
+    if(!nombre)return;
     /*if (nombre === ''){
 		return;
 	}
 	es equivalent al anterior*/ 
     contador++; 
     /*contador = contador + 1 es equivalente*/
-    const item = `<p class="historial-item"><strong>${contador}.</strong> Guardado: <em>${nombre}</em></p>`
-    historial.innerHTML +=item;
+    const item = `<p class="historial-item"><strong>${contador}.</strong> Guardado: <em>${nombre}</em></p>`;
+    historial.innerHTML += item;
 }
 
 function finalizar(){
     const nombre = document.getElementById('nombre').value.trim();
     const mensajeBase = nombre ? `Proceso finalizado.` : 'Proceso finalizado.';
-    alert(mensajeBase + 'El historial será limpiado.');
+    alert(mensajeBase + ' El historial será limpiado.');
+
     const historial = document.getElementById('historial');
     historial.innerHTML = '';
     contador = 0;
+    
 }
+
+const mensajeElemento = document.getElementById('mensaje');
+const cambiarMensajeBtn = document.getElementById('changeTextBtn');
+
+cambiarMensajeBtn.addEventListener('click', function() {
+    mensajeElemento.textContent = 'El mensaje ha sido cambiado por JavaScript';
+    mensajeElemento.style.color = 'green';
+    mensajeElemento.style.fontSize = '20px';
+
+    cambiarMensajeBtn.textContent = 'Mensaje Cambiado';
+    cambiarMensajeBtn.disabled = true;
+});
